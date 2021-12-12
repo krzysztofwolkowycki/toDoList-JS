c6c43b933c9b4355bb88a8724b6f6f5f7dd7f711
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2021-12-12 12:22:16 +0100

    FilterTask

--- main.js
+++ main.js
@@ -4,17 +4,22 @@ const inputAdd = document.querySelector('form input');
 const inputFilter = document.getElementById('searchTask');
 const numberTask = document.querySelector('h1 span');
 const ul = document.querySelector('ul');
-const liElements = document.querySelectorAll('li');
 
-const removeTask = (e)=>{
+const removeTask = (e) => {
     e.target.parentNode.remove();
-    
+    const index = e.target.parentNode.dataset.key;
+    taskArr.splice(index, 1);
+    console.log(taskArr);
+    renderTask();
+    numberTask.textContent = taskArr.length;
+
 }
 
-const addTask = (e)=>{
+const addTask = (e) => {
     e.preventDefault();
     const textTask = inputAdd.value;
     console.log(textTask);
+    if (textTask === '') return;
     const task = document.createElement('li');
     task.classList = 'task';
     task.textContent = textTask;
@@ -23,15 +28,26 @@ const addTask = (e)=>{
     ul.appendChild(task);
     inputAdd.value = '';
     numberTask.textContent = taskArr.length;
-    task.document.querySelector('button').addEventListener('click', removeTask);
-
+    task.querySelector('button').addEventListener('click', removeTask);
+    renderTask();
+}
+const renderTask = () => {
     ul.textContent = '';
-    taskArr.forEach((element, key)=>{
+    taskArr.forEach((element, key) => {
         element.dataset.key = key;
         ul.appendChild(element);
     })
 }
 
-
+const filterTask = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    let tasksSearch = [...document.querySelectorAll('li')];
+    console.log(tasksSearch);
+    tasksSearch = tasksSearch.filter(li => li.textContent.toLowerCase().includes(searchText));
+    console.log(tasksSearch);
+    ul.textContent = "";
+    tasksSearch.forEach(li => ul.appendChild(li));
+}
 
 form.addEventListener('submit', addTask);
+inputFilter.addEventListener('input', filterTask)
